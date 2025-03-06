26da2bc07d6c2f1695449c9f1f738092f05fef8e
Usow Maxim <usow.maxim2016@yandex.ru>
2025-03-06 08:03:22 +0700

    migrate 1.21.4

--- src/main/java/ru/justneed.learn121/Config.java
+++ src/main/java/ru/justneed.learn121/Config.java
@@ -14,7 +14,7 @@ import net.neoforged.neoforge.common.ModConfigSpec;
 
 // An example config class. This is not required, but it's a good idea to have one to keep your config organized.
 // Demonstrates how to use Neo's config APIs
-@EventBusSubscriber(modid = MyLearnMod.MOD_ID, bus = EventBusSubscriber.Bus.MOD)
+@EventBusSubscriber(modid = MyLearnMod.MODID, bus = EventBusSubscriber.Bus.MOD)
 public class Config
 {
     private static final ModConfigSpec.Builder BUILDER = new ModConfigSpec.Builder();
@@ -58,7 +58,7 @@ public class Config
 
         // convert the list of strings into a set of items
         items = ITEM_STRINGS.get().stream()
-                .map(itemName -> BuiltInRegistries.ITEM.get(ResourceLocation.parse(itemName)))
+                .map(itemName -> BuiltInRegistries.ITEM.getValue(ResourceLocation.parse(itemName)))
                 .collect(Collectors.toSet());
     }
 }
--- src/main/java/ru/justneed.learn121/MyLearnMod.java
+++ src/main/java/ru/justneed.learn121/MyLearnMod.java
@@ -17,10 +17,10 @@ import net.neoforged.neoforge.common.NeoForge;
 import net.neoforged.neoforge.event.BuildCreativeModeTabContentsEvent;
 import net.neoforged.neoforge.event.server.ServerStartingEvent;
 
-@Mod(MyLearnMod.MOD_ID)
+@Mod(MyLearnMod.MODID)
 public class MyLearnMod
 {
-    public static final String MOD_ID = "my_learn_mod";
+    public static final String MODID = "mylearnmod";
 
     private static final Logger LOGGER = LogUtils.getLogger();
 
@@ -28,23 +28,37 @@ public class MyLearnMod
     {
         modEventBus.addListener(this::commonSetup);
 
+
+
         NeoForge.EVENT_BUS.register(this);
+
+        modEventBus.addListener(this::addCreative);
 
         modContainer.registerConfig(ModConfig.Type.COMMON, Config.SPEC);
     }
 
+    private void commonSetup(final FMLCommonSetupEvent event)
+    {
+    }
 
-    private void commonSetup(final FMLCommonSetupEvent event) { }
+    private void addCreative(BuildCreativeModeTabContentsEvent event)
+    {
 
-    private void addCreative(BuildCreativeModeTabContentsEvent event) {}
+    }
 
     @SubscribeEvent
-    public void onServerStarting(ServerStartingEvent event) { }
+    public void onServerStarting(ServerStartingEvent event)
+    {
 
-    @EventBusSubscriber(modid = MOD_ID, bus = EventBusSubscriber.Bus.MOD, value = Dist.CLIENT)
+    }
+
+    @EventBusSubscriber(modid = MODID, bus = EventBusSubscriber.Bus.MOD, value = Dist.CLIENT)
     public static class ClientModEvents
     {
         @SubscribeEvent
-        public static void onClientSetup(FMLClientSetupEvent event) { }
+        public static void onClientSetup(FMLClientSetupEvent event)
+        {
+
+        }
     }
 }
